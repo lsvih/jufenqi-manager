287c66dd12c1b93f7bbd9e2e4f2143e6af4161e8
lyz <408772145@qq.com>
2016-11-06 23:53:48 +0800

    Fix plan image

--- src/module/zx-order/app.vue
+++ src/module/zx-order/app.vue
@@ -40,7 +40,7 @@
         <scroller lock-y scrollbar-x :height=".8*getScreenWidth()*.63+20+'px'" v-ref:plan>
             <div class="worker-product-list" :style="{width:order.planList[selectPlan].images.length*(.8*getScreenWidth()+10)+  'px',height:.8*getScreenWidth()*.63+'px'}">
                 <div class="worker-product-item" v-for="preview in order.planList[selectPlan].images" :style="{width: getScreenWidth()*.8 + 'px',height:.8*getScreenWidth()*.63+'px'}">
-                    <x-img class="product-img" :scroller="$refs.plan" :src="preview" v-tap="$refs.previewer.show($index)"></x-img>
+                    <x-img class="product-img" :scroller="$refs.plan" :src="imgUrl + preview" v-tap="$refs.previewer.show($index)"></x-img>
                 </div>
             </div>
         </scroller>
@@ -125,6 +125,7 @@ export default {
                 name: "订单已取消"
             }],
             selectPlan: 0,
+            imgUrl: Lib.C.imgUrl,
             options: {
                 getThumbBoundsFn(index) {
                     let thumbnail = document.querySelectorAll('.product-img')[index]
